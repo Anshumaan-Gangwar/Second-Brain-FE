2f762245e9cf5510c187cfe2e697e2f06c46b43b
Anshumaan-Gangwar <gangwaranshumaan@gmail.com>
2025-07-26 18:44:56 +0530

    added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import HomePage from "./pages/HomePage"
+import Auth from "./pages/Auth";
+import Dashboard from "./pages/Dashboard";
+
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/Signin", element: <Auth /> },
+  { path: "/Signup", element: <Auth /> },
+  { path: "/Dashboard", element: <Dashboard /> },
+  { path: "/share/:hash", element: <Dashboard /> },
+]);
 
 
 function App() {
   return (
-    <div className="text-red-500">hi there</div>
+    <div className="text-red-500 bg-amber-300">
+      <RouterProvider router={router}/>
+    </div>
   )
 }
 
